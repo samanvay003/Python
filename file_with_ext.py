426ca82f2654dbe7a979354fcb9ca675121da3c6
Samanvay Srivastava <31175288+samanvay003@users.noreply.github.com>
2022-11-08 19:49:56 +0530

    Create file_with_ext.py

---
+++ file_with_ext.py
@@ -0,0 +1,17 @@
+import os
+from unittest import result
+abs_path = input("Please enter the absolute path: ")
+
+if(os.path.isfile(abs_path)):
+    print(f"The path {abs_path} is not a directory ")
+else:
+    all_files_dir = os.listdir(abs_path)
+    if (len(all_files_dir) == 0):
+        print(f"There are no files in {abs_path} path")
+    else:
+        ext = input("Plese enter the extension")
+        result=[]
+        for i in all_files_dir:
+            if(i.endswith(ext)):
+                result.append(i)
+        print(result)
